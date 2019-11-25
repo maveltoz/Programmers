d14f734bfb3766565c415a6d0b0a4917ae58cb8f
Daehoon Kim <37270069+maveltoz@users.noreply.github.com>
2019-11-25 15:46:27 +0900

    2019-11-25

--- category/Greedy/큰 수 만들기.cpp
+++ category/Greedy/큰 수 만들기.cpp
@@ -34,3 +34,29 @@ int main() {
 
 	return 0;
 }
+
+/*
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string number, int k) {
+	string ans = "";
+	int len = number.size() - k;
+	int max_number(-1), index(-1);
+
+	for (int i = k; i < number.size(); i++) {
+		max_number = -1;
+		for (int j = index + 1; j <= i; j++) {
+			if (number[j] > max_number) {
+				max_number = number[j];
+				index = j;
+			}
+		}
+		ans += number[index];
+		number[index] = 0;
+	}
+
+	return ans;
+}
+*/
