0787dcabb41ce1750907069c762f690eb68450c1
Daehoon Kim <37270069+maveltoz@users.noreply.github.com>
2019-11-25 15:36:44 +0900

    2019-11-25

--- category/Greedy/저울.cpp
+++ category/Greedy/저울.cpp
@@ -53,20 +53,20 @@ int main() {
 using namespace std;
 
 int solution(vector<int> weight) {
-    int ans(0);
-    
-    sort(weight.begin(), weight.end());
-    
+	int ans(0);
+
+	sort(weight.begin(), weight.end());
+
 	if (weight[0] > 1) {
-        ans = 1;
-        return ans;
+		ans = 1;
+		return ans;
 	}
 
 	for (int i = 0; i < weight.size(); i++) {
 		ans += weight[i];
 		if (i != weight.size() - 1 && ans + 1 < weight[i + 1]) break;
 	}
-    
-    return ++ans;
+
+	return ++ans;
 }
 */
