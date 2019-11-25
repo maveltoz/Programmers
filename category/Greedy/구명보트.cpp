21ac4b5911d60e246f4bd0a942007544f1488b8e
Daehoon Kim <37270069+maveltoz@users.noreply.github.com>
2019-11-25 15:39:19 +0900

    2019-11-25

--- category/Greedy/구명보트.cpp
+++ category/Greedy/구명보트.cpp
@@ -42,3 +42,26 @@ int main() {
 
 	return 0;
 }
+
+/*
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+	int flag(0), ans(0);
+
+	sort(people.begin(), people.end());
+
+	for (int i = people.size() - 1; i >= flag; i--) {
+		if (people[flag] + people[i] <= limit) {
+			flag++;
+		}
+		ans++;
+	}
+
+	return ans;
+}
+*/
