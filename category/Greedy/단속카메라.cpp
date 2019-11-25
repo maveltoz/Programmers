f181cc140739838214f30a87066d93871a966836
Daehoon Kim <37270069+maveltoz@users.noreply.github.com>
2019-11-25 12:40:54 +0900

    2019-11-25

--- category/Greedy/단속카메라.cpp
+++ category/Greedy/단속카메라.cpp
@@ -50,29 +50,3 @@ int main() {
 
 	return 0;
 }
-
-/*
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-bool compare(vector<int> a, vector<int> b){
-    return a[1] < b[1];
-}
-
-int solution(vector<vector<int>> routes) {
-    int flag(-30001), ans(0);
-
-    sort(routes.begin(), routes.end(), compare);
-
-    for (int i = 0; i < routes.size(); i++) {
-        if (flag < routes[i][0]) {
-            flag = routes[i][1];
-            ans++;
-        }
-    }
-
-    return ans;
-}
-*/
