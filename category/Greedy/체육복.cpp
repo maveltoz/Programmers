85e60fa3845a2022c6c8947c966da6460639f2de
Daehoon Kim <37270069+maveltoz@users.noreply.github.com>
2019-11-25 15:43:20 +0900

    2019-11-24

--- category/Greedy/체육복.cpp
+++ category/Greedy/체육복.cpp
@@ -67,3 +67,40 @@ int main() {
 	
 	return 0;
 }
+
+/*
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+	int ans = n - lost.size();
+
+	for (int i = 0; i < lost.size(); i++) {
+		for (int j = 0; j < reserve.size(); j++) {
+			if (lost[i] == reserve[j]) {
+				lost[i] = -1;
+				reserve[j] = -1;
+				ans++;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < lost.size(); i++) {
+		int before = lost[i] - 1;
+		int after = lost[i] + 1;
+
+		for (int j = 0; j < reserve.size(); j++) {
+			if (reserve[j] == before || reserve[j] == after) {
+				reserve[j] = -1;
+				ans++;
+				break;
+			}
+		}
+	}
+
+	return ans;
+}
+*/
