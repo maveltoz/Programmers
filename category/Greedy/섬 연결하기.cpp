1b45a438ea51fdacce9ec6a82816dfc4a7ac6a22
Daehoon Kim <37270069+maveltoz@users.noreply.github.com>
2019-11-25 19:44:00 +0900

    2019-11-25

--- category/Greedy/섬 연결하기.cpp
+++ category/Greedy/섬 연결하기.cpp
@@ -78,34 +78,34 @@ using namespace std;
 vector<int> disjointSet;
 
 bool compare(vector<int> a, vector<int> b) {
-	return a[2] < b[2];
+    return a[2] < b[2];
 }
 
 int unionFind(int node) {
-	if (node == disjointSet[node]) return node;
-	else return disjointSet[node] = unionFind(disjointSet[node]);
+    if (node == disjointSet[node]) return node;
+    else return disjointSet[node] = unionFind(disjointSet[node]);
 }
 
 int solution(int n, vector<vector<int>> costs) {
-  int ans(0);
-    
-  disjointSet.resize(n);
-	for (int i = 0; i < n; i++) {
-		disjointSet[i] = i;
-	}
-	sort(costs.begin(), costs.end(), compare);
+    int ans(0);
 
-	for (int i = 0; i < costs.size(); i++) {
-		int start = unionFind(costs[i][0]);
-		int end = unionFind(costs[i][1]);
-		int cost = costs[i][2];
+    disjointSet.resize(n);
+    for (int i = 0; i < n; i++) {
+        disjointSet[i] = i;
+    }
+    sort(costs.begin(), costs.end(), compare);
 
-		if (start != end) {
-			disjointSet[start] = end;
-			ans += cost;
-		}
-	}
-    
+    for (int i = 0; i < costs.size(); i++) {
+        int start = unionFind(costs[i][0]);
+        int end = unionFind(costs[i][1]);
+        int cost = costs[i][2];
+
+        if (start != end) {
+            disjointSet[start] = end;
+            ans += cost;
+        }
+    }
+
     return ans;
 }
 */
