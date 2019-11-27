eca17f11a2bb86fa7dafc49e9fcde87ee4391def
Dae-Hoon, KIM <eogns0707@gmail.com>
2019-11-27 16:28:36 +0900

    2019-11-25

--- category/Greedy/저울.cpp
+++ category/Greedy/저울.cpp
@@ -2,6 +2,30 @@
 https://programmers.co.kr/learn/courses/30/lessons/42886
 */
 
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> weight) {
+	int ans(0);
+
+	sort(weight.begin(), weight.end());
+
+	if (weight[0] > 1) {
+		ans = 1;
+		return ans;
+	}
+
+	for (int i = 0; i < weight.size(); i++) {
+		ans += weight[i];
+		if (i != weight.size() - 1 && ans + 1 < weight[i + 1]) break;
+	}
+
+	return ++ans;
+}
+
+/*
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -44,29 +68,5 @@ int main() {
 	cout << ++ans;
 
 	return 0;
-}
-
-/*
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int solution(vector<int> weight) {
-	int ans(0);
-
-	sort(weight.begin(), weight.end());
-
-	if (weight[0] > 1) {
-		ans = 1;
-		return ans;
-	}
-
-	for (int i = 0; i < weight.size(); i++) {
-		ans += weight[i];
-		if (i != weight.size() - 1 && ans + 1 < weight[i + 1]) break;
-	}
-
-	return ++ans;
 }
 */
