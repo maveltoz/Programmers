a14653d931aa4f417c4b48a997437bad76ad067a
Dae-Hoon, KIM <eogns0707@gmail.com>
2019-11-27 16:27:45 +0900

    2019-11-25

--- category/Greedy/섬 연결하기.cpp
+++ category/Greedy/섬 연결하기.cpp
@@ -2,6 +2,46 @@
 https://programmers.co.kr/learn/courses/30/lessons/42861
 */
 
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> disjointSet;
+
+bool compare(vector<int> a, vector<int> b) {
+    return a[2] < b[2];
+}
+
+int unionFind(int node) {
+    if (node == disjointSet[node]) return node;
+    else return disjointSet[node] = unionFind(disjointSet[node]);
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int ans(0);
+
+    disjointSet.resize(n);
+    for (int i = 0; i < n; i++) {
+        disjointSet[i] = i;
+    }
+    sort(costs.begin(), costs.end(), compare);
+
+    for (int i = 0; i < costs.size(); i++) {
+        int start = unionFind(costs[i][0]);
+        int end = unionFind(costs[i][1]);
+        int cost = costs[i][2];
+
+        if (start != end) {
+            disjointSet[start] = end;
+            ans += cost;
+        }
+    }
+
+    return ans;
+}
+
+/*
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -67,46 +107,6 @@ int main() {
 	cout << ans;
 
 	return 0;
-}
-
-/*
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-vector<int> disjointSet;
-
-bool compare(vector<int> a, vector<int> b) {
-    return a[2] < b[2];
-}
-
-int unionFind(int node) {
-    if (node == disjointSet[node]) return node;
-    else return disjointSet[node] = unionFind(disjointSet[node]);
-}
-
-int solution(int n, vector<vector<int>> costs) {
-    int ans(0);
-
-    disjointSet.resize(n);
-    for (int i = 0; i < n; i++) {
-        disjointSet[i] = i;
-    }
-    sort(costs.begin(), costs.end(), compare);
-
-    for (int i = 0; i < costs.size(); i++) {
-        int start = unionFind(costs[i][0]);
-        int end = unionFind(costs[i][1]);
-        int cost = costs[i][2];
-
-        if (start != end) {
-            disjointSet[start] = end;
-            ans += cost;
-        }
-    }
-
-    return ans;
 }
 */
 
