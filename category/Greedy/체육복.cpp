8c82c64667ea2a12c85791d2700ea50aafbe8fc3
Dae-Hoon, KIM <eogns0707@gmail.com>
2019-11-27 16:29:34 +0900

    2019-11-25

--- category/Greedy/체육복.cpp
+++ category/Greedy/체육복.cpp
@@ -2,6 +2,41 @@
 https://programmers.co.kr/learn/courses/30/lessons/4286
 */
 
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+	int ans = n - lost.size();
+
+	for (int i = 0; i < lost.size(); i++) {
+		for (int j = 0; j < reserve.size(); j++) {
+			if (lost[i] == reserve[j]) {
+				lost[i] = -1;
+				reserve[j] = -1;
+				ans++;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < lost.size(); i++) {
+		int before = lost[i] - 1;
+		int after = lost[i] + 1;
+
+		for (int j = 0; j < reserve.size(); j++) {
+			if (reserve[j] == before || reserve[j] == after) {
+				reserve[j] = -1;
+				ans++;
+				break;
+			}
+		}
+	}
+
+	return ans;
+}
+
+/*
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -66,40 +101,5 @@ int main() {
 	cout << ans;
 	
 	return 0;
-}
-
-/*
-#include <string>
-#include <vector>
-using namespace std;
-
-int solution(int n, vector<int> lost, vector<int> reserve) {
-	int ans = n - lost.size();
-
-	for (int i = 0; i < lost.size(); i++) {
-		for (int j = 0; j < reserve.size(); j++) {
-			if (lost[i] == reserve[j]) {
-				lost[i] = -1;
-				reserve[j] = -1;
-				ans++;
-				break;
-			}
-		}
-	}
-
-	for (int i = 0; i < lost.size(); i++) {
-		int before = lost[i] - 1;
-		int after = lost[i] + 1;
-
-		for (int j = 0; j < reserve.size(); j++) {
-			if (reserve[j] == before || reserve[j] == after) {
-				reserve[j] = -1;
-				ans++;
-				break;
-			}
-		}
-	}
-
-	return ans;
 }
 */
