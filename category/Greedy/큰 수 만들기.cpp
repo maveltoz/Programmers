2ad3b58c3e476526b0f85584c5f2a20848dc2dca
Dae-Hoon, KIM <eogns0707@gmail.com>
2019-11-27 16:30:13 +0900

    2019-11-25

--- category/Greedy/큰 수 만들기.cpp
+++ category/Greedy/큰 수 만들기.cpp
@@ -2,6 +2,31 @@
 https://programmers.co.kr/learn/courses/30/lessons/42883
 */
 
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string number, int k) {
+	string ans = "";
+	int len = number.size() - k;
+	int max_number(-1), index(-1);
+
+	for (int i = k; i < number.size(); i++) {
+		max_number = -1;
+		for (int j = index + 1; j <= i; j++) {
+			if (number[j] > max_number) {
+				max_number = number[j];
+				index = j;
+			}
+		}
+		ans += number[index];
+		number[index] = 0;
+	}
+
+	return ans;
+}
+
+/*
 #include <iostream>
 #include <vector>
 #include <string>
@@ -33,30 +58,5 @@ int main() {
 	cout << ans;
 
 	return 0;
-}
-
-/*
-#include <string>
-#include <vector>
-using namespace std;
-
-string solution(string number, int k) {
-	string ans = "";
-	int len = number.size() - k;
-	int max_number(-1), index(-1);
-
-	for (int i = k; i < number.size(); i++) {
-		max_number = -1;
-		for (int j = index + 1; j <= i; j++) {
-			if (number[j] > max_number) {
-				max_number = number[j];
-				index = j;
-			}
-		}
-		ans += number[index];
-		number[index] = 0;
-	}
-
-	return ans;
 }
 */
